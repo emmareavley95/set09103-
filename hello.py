f41847e7006ba8e402aebd60efe36b30bb1fe927
Emma Reavley <emma.reavley@wanadoo.fr>
2016-10-10 10:13:04 +0000

    add new files tutorial chapter 5

--- hello.py
+++ hello.py
@@ -1,9 +1,31 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def root():
   return "Hello Emma!"
+
+@app.route('/inherits/')
+def inherits():
+  return render_template('base.html')
+
+@app.route('/inherits/one/')
+def inherits_one():
+  return render_template('inherits1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+  return render_template('inherits2.html')
+
+@app.route('/users/')
+def users():
+  names = ['simon', 'thomas', 'lee', 'jamie', 'sylvester']
+  return render_template('loops.html', names=names)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('conditional.html', name=name)
 
 @app.route("/display/")
 def display():
